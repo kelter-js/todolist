6bb69a78e8cbce43755e291f53965eaa3a4b0dcb
kelter-js <spawnkillerqw@gmail.com>
2024-09-24 18:59:50 +0500

    feat: created more test data

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,22 @@
 import { v1 } from "uuid";
 
-export const tasks = [
-  { id: v1(), description: 'CSS', isDone: true },
-  { id: v1(), description: 'HTML', isDone: true },
-  { id: v1(), description: 'JS & React', isDone: true },
-  { id: v1(), description: 'React Native', isDone: false },
+export const firstTasks = [
+  { id: v1(), description: "CSS", isDone: true },
+  { id: v1(), description: "HTML", isDone: true },
+  { id: v1(), description: "JS & React", isDone: true },
+  { id: v1(), description: "React Native", isDone: false },
+];
+
+export const secondTasks = [
+  { id: v1(), description: "CSS", isDone: true },
+  { id: v1(), description: "HTML", isDone: true },
+  { id: v1(), description: "JS & React", isDone: true },
+  { id: v1(), description: "React Native", isDone: false },
+];
+
+export const thirdTasks = [
+  { id: v1(), description: "CSS", isDone: true },
+  { id: v1(), description: "HTML", isDone: true },
+  { id: v1(), description: "JS & React", isDone: true },
+  { id: v1(), description: "React Native", isDone: false },
 ];
